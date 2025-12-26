6c029afcbdebdf9edff08c828c72b891fcd8ad18
tzl21 <2869974409@qq.com>
2025-12-26 16:00:53 +0800

    add gdal processor for check_data

--- utils/utils.py
+++ utils/utils.py
@@ -100,7 +100,7 @@ def check_loaded_dataset(work_dir='./', print_msg=True, relpath=False):
             obj.open(print_msg=False)
 
             # get the proper lookup table dataset names
-            if processor in ['isce', 'doris']:
+            if processor in ['isce', 'doris','gdal']:
                 dnames = GEOMETRY_DSET_NAMES[1:3]
             elif processor in ['gamma', 'roipac']:
                 dnames = GEOMETRY_DSET_NAMES[3:5]
